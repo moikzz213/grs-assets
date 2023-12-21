c69bc4a778e9aa6ee942ca6e663202049011b9f5
moikzz213 <jacob@gagroup.net>
2023-12-21 14:34:43 +0400

    update margin

--- public/build/assets/NewData-7835b6ff.js
+++
@@ -1,1 +0,0 @@
-import{p as e,d as t,o as r}from"./app-6ea0d241.js";import{_ as a}from"./DataForm-537b17c6.js";import"./ApppageHeader-a507d209.js";import"./Studio-b67fc4d7.js";import"./AppSnackBar-b3384ecd.js";import"./formatDate-b9aad8c5.js";const f={__name:"NewData",setup(o){return(s,m)=>(r(),e("div",null,[t(a,{headertitle:"Transfer Assets"})]))}};export{f as default};
--- resources/js/pages/assets/Assets.vue
+++ resources/js/pages/assets/Assets.vue
@@ -348,10 +348,10 @@
             :backgroundOptions="{ color: '#ffffff' }"
             :cornersSquareOptions="{ type: 'square', color: '#0e013c' }" 
             fileExt="svg" 
-            myclass="my-qur font-weight-bold pl-2 mb-1" 
+            myclass="my-qur font-weight-bold pl-2 mt-1" 
             imgclass="img-qr" 
           /> 
-          <strong  style="font-size:20px; width: 100%; text-align: center;margin-bottom:20px;">{{ item }}</strong>
+          <strong  style="font-size:20px; width: 100%; text-align: center;margin-top:10px;">{{ item }}</strong>
       </div>
     </div>
   </div>
