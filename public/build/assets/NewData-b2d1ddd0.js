6680e7a33ba4307f5434ca3042de3be70de5b6f7
moikzz213 <jacob@gagroup.net>
2024-02-22 16:40:03 +0400

    updated

--- public/build/assets/NewData-b2d1ddd0.js
+++
@@ -1,1 +0,0 @@
-import{p as e,d as t,o as r}from"./app-24423a79.js";import{_ as o}from"./DataForm-caf731e3.js";import"./AppPageHeader-248ca31a.js";import"./Studio-cbf20b03.js";import"./AppSnackBar-53ced0c6.js";import"./formatDate-b9aad8c5.js";const u={__name:"NewData",setup(a){return(s,m)=>(r(),e("div",null,[t(o,{headertitle:"Request Assets"})]))}};export{u as default};
