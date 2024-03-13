7a78691086f2fd032515aeb543406a533e4216d4
moikzz213 <jacob@gagroup.net>
2024-03-13 14:10:28 +0400

    uom dropdown

--- public/build/assets/categories-1460b498.js
+++
@@ -1,1 +0,0 @@
-import{ar as a,s}from"./app-3951fe92.js";const r=a("categories",{state:()=>({categories:[]}),getters:{list:e=>e.categories},actions:{async getCategories(e){await s(e).get("/api/category/state/category-list").then(t=>{this.categories=Object.assign([],t.data)}).catch(t=>{console.log("getCategories error: ",t)})}}});export{r as u};
