d5f13b9b17a4db003f31cd7aa27c0d8f98646f6d
moikzz213 <jacob@gagroup.net>
2024-03-13 14:20:24 +0400

    disabled weight and value

--- public/build/assets/NewData-fd2cee07.js
+++
@@ -1,1 +0,0 @@
-import{p as e,d as t,o as r}from"./app-deec0813.js";import{_ as a}from"./DataForm-ab12c1f2.js";import"./ApppageHeader-fdcc78e4.js";import"./Studio-780e6e23.js";import"./AppSnackBar-892c696c.js";import"./formatDate-b9aad8c5.js";const f={__name:"NewData",setup(o){return(s,m)=>(r(),e("div",null,[t(a,{headertitle:"Transfer Assets"})]))}};export{f as default};
