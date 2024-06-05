8eba722600e5a52cdb087f0a6f73fcce5da83e3e
moikzz213 <jacob@gagroup.net>
2024-06-05 11:13:48 +0400

    fixed job

--- app/Jobs/TestJob.php
+++ app/Jobs/TestJob.php
@@ -12,7 +12,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 
-class CronJobAssetRequest implements ShouldQueue
+class TestJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels; 
   
